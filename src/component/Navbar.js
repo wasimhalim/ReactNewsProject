9774f748b0e2f64d1b9141dc9d7404c81b82937c
wasimhalim <m.wasimhalim@gmail.com>
2021-02-11 01:21:09 -0800

    new Project created

---
+++ src/component/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className="w-full h-8 bg-gray-600">
+            <div className="text-center p-1">
+            <a href="" className="text-white ">News Website</a>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/component/News.js
@@ -0,0 +1,30 @@
+import React,{useState,useEffect} from 'react'
+import axios from "axios"
+
+function News() {
+    const [content,setContent]=useState("")
+    const [news,setNews]=useState([])
+    const [count,setCount]=useState(0)
+    useEffect(()=>{
+        axios.get("http://newsapi.org/v2/top-headlines?country=us&apiKey=14e88762b4e84ef38beb993992ac328f")
+        .then(res=>{
+            console.log(res.data.articles)
+            setNews(res.data.articles)
+        })
+    },[])
+    return (
+        <div className="max-w-5xl h-full  mx-auto mt-5 rounded-md flex flex-row flex-wrap">
+          {news.map(new_=>(
+              
+              <div className="mb-3 bg-gray-300 hover:bg-gray-100  m-2 p-4 rounded-md w-52" key={new_.url} onClick={()=>window.open(new_.url,"_blank")}>
+                  <h6 className="text-sm">{new_.title}</h6>
+                    <p className="text-xs mb-2">{new_.content}</p>   
+                    <p className="text-xs">{new_.author  ? "Author: "+new_.author :"" }</p>   
+              </div>
+            
+          ))}
+        </div>
+    )
+}
+
+export default News
